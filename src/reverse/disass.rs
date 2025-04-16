50d4fe654f1a8deb12f9a18e079c14282f4e59e9
Ectario <ectario.contact@gmail.com>
2025-04-16 18:00:09 +0200

    CFG with strings repr done

--- src/reverse/disass.rs
+++ src/reverse/disass.rs
@@ -1,10 +1,9 @@
 use solana_sbpf::{
-    elf::Executable,
-    program::{BuiltinProgram, FunctionRegistry, SBPFVersion},
-    static_analysis::Analysis,
+    ebpf::Insn, elf::Executable, program::{BuiltinProgram, FunctionRegistry, SBPFVersion}, static_analysis::Analysis
 };
-use std::{fmt::write, sync::Arc};
+use std::{sync::Arc, u8};
 use test_utils::TestContextObject;
+use std::collections::{BTreeMap, HashSet};
 
 use std::fs::File;
 use std::io::{Result, Write};
@@ -12,40 +11,196 @@ use std::path::Path;
 use std::fmt::Write as _; // for write! on String
 use crate::reverse::ReverseOutputMode;
 
-pub fn export_cfg_to_dot<P: AsRef<Path>>(analysis: &Analysis, path: P) -> Result<()> {
-    let mut file = File::create(path)?;
+const MAX_BYTES_TO_READ_FOR_IMMUTABLE_STRING_REPR: u8 = 50;
 
-    writeln!(file, "digraph CFG {{")?;
-    writeln!(file, "    node [shape=box, fontname=\"monospace\"];")?;
+fn get_string_repr(program: &'static [u8], insn: &Insn, next_insn_wrapped: Option<&Insn>, analysis: &Analysis) -> String {
+    match insn.opc {
+        solana_sbpf::ebpf::LD_DW_IMM => {
+            let start = insn.imm as usize;
 
-    // Write all nodes with instructions
-    for (id, node) in &analysis.cfg_nodes {
-        // Start with the block ID and label
-        let mut label = format!("BB{id}\\n{}", node.label.replace("\"", "\\\""));
+            // Default number of bytes to read from the program when no length found
+            let mut length = MAX_BYTES_TO_READ_FOR_IMMUTABLE_STRING_REPR as usize;
 
-        // Add eBPF instructions for the block
-        for i in node.instructions.clone() {
-            if let Some(insn) = analysis.instructions.get(i) {
-                // Disassemble and escape double quotes for DOT format
-                let dis = analysis.disassemble_instruction(insn, i).replace('"', "\\\"");
-                let _ = write!(label, "\\n{:04}", insn.opc);
-                let _ = write!(label, "\\n{:04}: {}", i, dis);
+            if let Some(next_insn) = next_insn_wrapped {
+                if next_insn.opc == solana_sbpf::ebpf::MOV64_IMM || next_insn.opc == solana_sbpf::ebpf::MOV32_IMM {
+                    let maybe_len = next_insn.imm as usize;
+                    if maybe_len > 0 {
+                        length = maybe_len;
+                    }
+                }
             }
+
+            // Make sure we don't read out of bounds
+            if start >= program.len() {
+                return "".to_string();
+            }
+
+            let end = usize::min(start + length, program.len());
+            let slice = &program[start..end];
+            let mut bytes_repr = String::from(" b\"");
+
+            // Render printable ASCII as-is, otherwise use hex escape
+            for &b in slice {
+                if b.is_ascii_graphic() || b == b' ' {
+                    bytes_repr.push(b as char);
+                } else {
+                    write!(&mut bytes_repr, "\\x{:02x}", b).unwrap();
+                }
+            }
+
+            bytes_repr.push('"');
+            bytes_repr
         }
 
-        // Write the node to the DOT file
-        writeln!(file, "    {} [label=\"{}\"];", id, label)?;
+        _ => "".to_string(),
     }
+}
 
-    // Write all control-flow edges
-    for (id, node) in &analysis.cfg_nodes {
-        for &dest in &node.destinations {
-            writeln!(file, "    {} -> {};", id, dest)?;
+
+// modified version of "visualize_graphically" from sbpf-solana for full static analysis
+pub fn export_cfg_to_dot<P: AsRef<Path>>(
+    program: &'static [u8],
+    analysis: &mut Analysis,
+    path: P,
+) -> std::io::Result<()> {
+    let mut output = File::create(path)?;
+    fn html_escape(string: &str) -> String {
+        string
+            .replace('&', "&amp;")
+            .replace('<', "&lt;")
+            .replace('>', "&gt;")
+            .replace('\"', "&quot;")
+    }
+    fn emit_cfg_node<W: std::io::Write>(
+        program: &'static [u8],
+        output: &mut W,
+        analysis: &Analysis,
+        function_range: std::ops::Range<usize>,
+        alias_nodes: &mut HashSet<usize>,
+        cfg_node_start: usize,
+    ) -> std::io::Result<()> {
+        let cfg_node = &analysis.cfg_nodes[&cfg_node_start];
+        let insns = analysis.instructions[cfg_node.instructions.clone()].to_vec();
+        writeln!(output, "    lbb_{} [label=<<table border=\"0\" cellborder=\"0\" cellpadding=\"3\">{}</table>>];",
+            cfg_node_start,
+            analysis.instructions[cfg_node.instructions.clone()].iter()
+            .enumerate().map(|(pc, insn)| {
+                let mut desc = analysis.disassemble_instruction(
+                    insn, pc
+                );
+                // next instruction lookup to gather information (like for string and their length when it uses MOV64_IMM)
+                let next_insn = insns.get(pc + 1);
+                // add immutable string repr if it does exists on bytecode 
+                desc.push_str(&get_string_repr(program, insn, next_insn, &analysis));
+                if let Some(split_index) = desc.find(' ') {
+                    let mut rest = desc[split_index+1..].to_string();
+                    if rest.len() > MAX_CELL_CONTENT_LENGTH + 1 {
+                        rest.truncate(MAX_CELL_CONTENT_LENGTH);
+                        rest = format!("{rest}…");
+                    }
+                    format!("<tr><td align=\"left\">{}</td><td align=\"left\">{}</td></tr>", html_escape(&desc[..split_index]), html_escape(&rest))
+                } else {
+                    format!("<tr><td align=\"left\">{}</td></tr>", html_escape(&desc))
+                }
+            })
+            .collect::<String>()
+        )?;
+        for child in &cfg_node.dominated_children {
+            emit_cfg_node(
+                program,
+                output,
+                analysis,
+                function_range.clone(),
+                alias_nodes,
+                *child,
+            )?;
+        }
+        Ok(())
+    }
+    writeln!(
+        output,
+        "digraph {{
+graph [
+rankdir=LR;
+concentrate=True;
+style=filled;
+color=lightgrey;
+];
+node [
+shape=rect;
+style=filled;
+fillcolor=white;
+fontname=\"Courier New\";
+];
+edge [
+fontname=\"Courier New\";
+];"
+    )?;
+    const MAX_CELL_CONTENT_LENGTH: usize = 15 + MAX_BYTES_TO_READ_FOR_IMMUTABLE_STRING_REPR as usize;
+    let function_iter = &mut analysis.functions.keys().peekable();
+    while let Some(function_start) = function_iter.next() {
+        let function_end = if let Some(next_function) = function_iter.peek() {
+            **next_function
+        } else {
+            &analysis.instructions.last().unwrap().ptr + 1
+        };
+        let mut alias_nodes = HashSet::new();
+        writeln!(output, "  subgraph cluster_{} {{", *function_start)?;
+        writeln!(
+            output,
+            "    label={:?};",
+            html_escape(&analysis.cfg_nodes[function_start].label)
+        )?;
+        writeln!(output, "    tooltip=lbb_{};", *function_start)?;
+        emit_cfg_node(
+            program,
+            &mut output,
+            &analysis,
+            *function_start..function_end,
+            &mut alias_nodes,
+            *function_start,
+        )?;
+        for alias_node in alias_nodes.iter() {
+            writeln!(
+                output,
+                "    alias_{}_lbb_{} [",
+                *function_start, *alias_node
+            )?;
+            writeln!(output, "        label=lbb_{:?};", *alias_node)?;
+            writeln!(output, "        tooltip=lbb_{:?};", *alias_node)?;
+            writeln!(output, "        URL=\"#lbb_{:?}\";", *alias_node)?;
+            writeln!(output, "    ];")?;
+        }
+        writeln!(output, "  }}")?;
+    }
+    for (_, cfg_node_start, cfg_node) in analysis.iter_cfg_by_function() {
+        if cfg_node_start != cfg_node.dominator_parent {
+            writeln!(
+                output,
+                "  lbb_{} -> lbb_{} [style=dotted; arrowhead=none];",
+                cfg_node_start, cfg_node.dominator_parent,
+            )?;
+        }
+        let edges: BTreeMap<usize, usize> = cfg_node
+            .destinations
+            .iter()
+            .map(|destination| (*destination, 0))
+            .collect();
+        let counter_sum: usize = edges.values().sum();
+        if counter_sum == 0 && !edges.is_empty() {
+            writeln!(
+                output,
+                "  lbb_{} -> {{{}}};",
+                cfg_node_start,
+                edges
+                    .keys()
+                    .map(|destination| format!("lbb_{}", *destination))
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            )?;
         }
     }
-
-    writeln!(file, "}}")?;
-
+    writeln!(output, "}}")?;
     Ok(())
 }
 
@@ -61,19 +216,19 @@ pub fn analyze_program(program: &'static [u8], mode: ReverseOutputMode) -> Resul
     ).unwrap();
 
     // Perform analysis on the executable (e.g., disassembly, control flow graph).
-    let analysis = Analysis::from_executable(&executable).unwrap();
+    let mut analysis = Analysis::from_executable(&executable).unwrap();
     match mode {
         ReverseOutputMode::Disassembly => {
             let stdout = std::io::stdout();
             analysis.disassemble(&mut stdout.lock())?;
         }
         ReverseOutputMode::ControlFlowGraph(path) => {
-            export_cfg_to_dot(&analysis, &path)?;
+            export_cfg_to_dot(program, &mut analysis, &path)?;
         }
         ReverseOutputMode::DisassemblyAndCFG(path) => {
             let stdout = std::io::stdout();
             analysis.disassemble(&mut stdout.lock())?;
-            export_cfg_to_dot(&analysis, &path)?;
+            export_cfg_to_dot(program, &mut analysis, &path)?;
         }
         ReverseOutputMode::DisassAndRustEquivalent(path) => {
             println!("Rust equivalent generation is not implemented yet.");
@@ -1845,17 +2000,6 @@ fn test() {
         0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00
       ];
-    let loader = Arc::new(BuiltinProgram::new_mock());
-    let executable = Executable::<TestContextObject>::from_text_bytes(
-        program,
-        loader,
-        SBPFVersion::V3,
-        FunctionRegistry::default(),
-    )
-    .unwrap();
-    let analysis = Analysis::from_executable(&executable).unwrap();
-    let stdout = std::io::stdout();
-    // export_cfg_to_dot(&analysis, "MY_TEST/cfg.dot").unwrap();
-    let _ = analysis.visualize_graphically(&mut stdout.lock(), None);
-    // analysis.disassemble(&mut stdout.lock()).unwrap();
+
+    let _ = analyze_program(program, ReverseOutputMode::DisassemblyAndCFG("MY_TEST/full_cfg.dot".to_string()));
 }
